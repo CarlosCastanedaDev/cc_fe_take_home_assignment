02f087d29dd3e6ae9c65b8a90bfa66cee4627671
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2024-01-21 16:21:18 -0600

    fixed desktop view

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById('mySidenav').style.width = '300px';
-  document.getElementById('main').style.marginLeft = '300px';
-  document.querySelector('.illustration').classList.add('smaller');
+  if (window.innerWidth > 600) {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  } else {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '0';
+  }
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
-  document.querySelector('.illustration').classList.remove('smaller');
 }
