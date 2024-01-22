ce44f468ec84a4ba9e3172bfd36ac7b5555c88f1
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2024-01-21 18:02:02 -0600

    final changes and refactoring

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   if (window.innerWidth > 600) {
-    document.getElementById('mySidenav').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('mySidenav').style.width = '300px';
+    document.getElementById('main').style.marginLeft = '300px';
   } else {
     document.getElementById('mySidenav').style.width = '150px';
     document.getElementById('main').style.marginLeft = '125px';
